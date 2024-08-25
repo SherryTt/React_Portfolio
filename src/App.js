b7a40a6615fd2cfb2f376846a2fa83c4b77ac10a
Shiori <shiori0921syd@gmail.com>
2024-08-25 23:47:16 +1000

    Added banner section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nabvar } from './components/Navbar';
+import { Banner } from './components/Banner'
 
 function App() {
   return (
     <div className="App">
+      <Nabvar />
+      <Banner />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container,Nav } from "react-bootstrap";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
 export const Nabvar = () => {
 	const [activeLink, setActiveLink] = useState('home');
 	const [scrolled, setScrolled ] = useState(false);
@@ -26,7 +31,7 @@ export const Nabvar = () => {
 		<Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
 		<Container>
 		  <Navbar.Brand href="#home">
-			  <img src={''} alt="Logo" />
+			  <img src={logo} alt="Logo" />
 		  </Navbar.Brand>
 		  <Navbar.Toggle aria-controls="basic-navbar-nav" >
 			  <span className="navbar-toggle-icon"></span>
@@ -39,9 +44,9 @@ export const Nabvar = () => {
 			</Nav>
 			<span className="navbar-text">
 				<div className="social-icon">
-					<a href=""><img src={} alt="" /></a>
-					<a href=""><img src={} alt="" /></a>
-					<a href=""><img src={} alt="" /></a>
+					<a href=""><img src={navIcon1} alt="" /></a>
+					<a href=""><img src={navIcon2} alt="" /></a>
+					<a href=""><img src={navIcon3} alt="" /></a>
 				</div>
 				<button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
 			</span>
