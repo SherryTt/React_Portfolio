c36db01b33750b9c3a8f4d7459abe1c6964eb81a
Shiori <shiori0921syd@gmail.com>
2024-08-26 23:41:02 +1000

    Added Skills Section

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Nabvar } from './components/Navbar';
-import { Banner } from './components/Banner'
+import { Banner } from './components/Banner';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Nabvar />
       <Banner />
+      <Skills />
     </div>
   );
 }
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -47,8 +47,9 @@ export const Banner = () => {
 				<Row className="align-items-center">
 					<Col xs={12} md={6} xl={7}>
 						<span className="tagline">Welcome to my Portfolio</span>
-						<h1>{"Hi I'm webdecoded"}<span className="wrap">{text}</span></h1>
-						<p>Dummy text here aaaaaaaaaaaaaaaaaaaa</p>
+						<h1>{"Hi I'm webdecoded"}</h1>
+						<h1><span className="wrap">{text}</span></h1>
+						<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
 						<button onClick={ () => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
 					</Col>
 					<Col xs={12} md={6} xl={5}>
